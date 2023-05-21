ee55f90a67aa120228212ac32ed5069181741316
Átila <112980460+Relieff@users.noreply.github.com>
2023-05-21 12:00:32 -0300

    Update Media Aritimetica v2.py

--- Media Aritimetica v2.py
+++ Media Aritimetica v2.py
@@ -2,10 +2,10 @@ from time import sleep
 from math import trunc
 
 resposta = 'S'
-nota = final = menor = maior = 0
+nota = final = 0
 listanum = []
-menor = maior = 0
 
+print('Olá Professor(a), a nota para o aluno ter média é 15.0 pontos. \n')
 while resposta == 'S':
     nota = listanum.append(float(input('Digite a nota do aluno: ')))   #Adicionei todo numero digitado em uma lista
     print('Deseja continuar \n[S] Sim [N] Não')
@@ -14,12 +14,12 @@ while resposta == 'S':
     if resposta == 'N':
         break
 
-    if final < 15:
-        menor == final
-    else:
-        maior == final
-        
 final = sum(listanum)/2 #Somar os números da lista e dividir por 2.
-  
+
+if final >= 15:
+    print('O aluno em questão está na média.')
+else:
+    print('O aluno em questão está de recuperação.')        
+    
+
 print(f'A nota final do aluno aplicando a média foi de: {final}')
-print(f'A menor nota foi {menor} e a maoir nota foi {maior}')
